0c0b21c8898fdab19270d33011d03ba11efa3299
Bruno Costa Mendonça <1161016@isep.ipp.pt>
2019-09-04 16:25:42 +0100

    icon and title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mean-contactlist-angular2';
-}
+  public constructor(private titleService: Title) {
+    this.titleService.setTitle('Dark Order')
+  }}
